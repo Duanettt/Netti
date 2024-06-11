77faff0b2d31323ad38e7a5c3d6422f5ddeb0f12
Duanettt <dnettey28@gmail.com>
2024-06-11 02:05:43 +0100

    Working through renderer!

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -4,6 +4,22 @@
 // settings
 const unsigned int screenWidth = 800;
 const unsigned int screenHeight = 600;
+int  success;
+char infoLog[512];
+
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
 
 
 int main()
@@ -21,6 +37,36 @@ int main()
     // We then bind our buffer so we place our bufferdata into it.
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 
+
+
+    // create shaders
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    };
+
+
 
     GLFW::InitializeWindow(screenWidth, screenHeight, "LearnOpenGL");
 
@@ -28,6 +74,7 @@ int main()
     {
         GLFW::ProcessInput();
         Renderer::RunEvents();
+
         Renderer::Render();
 
 
@@ -47,5 +94,7 @@ void Renderer::Render()
     glClearColor(0.17f, 0.17f, 0.17f, 0.17f);
     glClear(GL_COLOR_BUFFER_BIT);
     // Render your objects here
+
+
 
 }
