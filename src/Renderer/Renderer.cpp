8ae060b4012beb49e0fc9dceefc45f0e65dbefa5
Duanettt <dnettey28@gmail.com>
2024-06-15 23:24:16 +0100

    Updated, 2D Renderer basically built

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -7,12 +7,15 @@ const unsigned int screenHeight = 600;
 int  success;
 char infoLog[512];
 
-const char* vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+float vertices[] = {
+    // positions         // colors
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+};
+unsigned int indices[] = {  // note that we start from 0!
+    0, 1, 2,   // first triangle
+};
 
 const char* fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
